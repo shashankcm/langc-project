501dace7ac5e7dbab7e4e01331ff5dbc315b85b0
Shashank Gupta <shashankgupta@MacBook-Air-2.local>
2026-08-04 21:58:45 -0400

    pdf loader

---
+++ src/document_loaders/document_loader.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+from pathlib import Path
+
+from langchain_community.document_loaders import (
+    TextLoader,
+    PyPDFLoader
+)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def load_text_file():
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(b"Hello this is sample text file.\nThis file will be deleted after use.")
+        temp_file_path = temp_file.name
+
+    try:
+        loader = TextLoader(temp_file_path, encoding="utf-8")
+        documents = loader.load()
+        for doc in documents:
+            print(doc.page_content)
+    finally:
+        os.remove(temp_file_path)
+
+def load_pdf_file(pdf_path: str):
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+
+    print(f"Loaded {len(documents)} page(s) from PDF.")
+
+    for i, doc in enumerate(documents):
+        print(f"Page {i + 1}:\n{doc.page_content}\n")
+        print(f"Metadata: {doc.metadata}\n")
+
+if __name__ == "__main__":
+    #load_text_file()
+    load_pdf_file("src/documents/001-HIDE-AND-SEEK-Free-Childrens-Book-By-Monkey-Pen.pdf")
--- src/document_loaders/pdf_loader.py
+++
@@ -1,8 +0,0 @@
-import os
-import tempfile
-from pathlib import Path
-from langchain_community.document_loaders import PyPDFLoader
-
-from dotenv import load_dotenv
-
-load_dotenv()
