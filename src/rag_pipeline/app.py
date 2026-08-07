0c773f0ef0ec3f6521631634286eefeb27ae67cc
Shashank Gupta <shashankgupta@MacBook-Air-2.local>
2026-08-07 11:38:08 -0400

    RAG pipeline setup

---
+++ src/rag_pipeline/app.py
@@ -0,0 +1,21 @@
+"""
+Complete implementation of Building RAG Pipeline.
+"""
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chat_models import init_chat_model
+
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pydantic import BaseModel, Field
+from typing import List
+from dotenv import load_dotenv
+
+import tempfile
+
+load_dotenv()
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
